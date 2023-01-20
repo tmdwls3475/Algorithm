6b5c6a9b06d2ffe346d55f7b5c89be23c8606bbe
0332330 <tmdwls8885@naver.com>
2023-01-20 19:22:34 +0900

    Update BOJ 9372.py

--- BOJ 9372.py
+++ BOJ 9372.py
@@ -1,1 +1,13 @@
 # 그래프, 트리
+
+import sys
+
+n = int(sys.stdin.readline())
+
+for _ in range(n):
+    N, M = map(int, sys.stdin.readline().split())
+    
+    for _ in range(M):
+        a, b = map(int,sys.stdin.readline().split())
+    
+    print(n - 1)
