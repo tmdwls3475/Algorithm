dc88d936c2686c6ac2b8ffd3dbad2ca368aa7147
0332330 <tmdwls8885@naver.com>
2023-01-26 22:33:40 +0900

    정렬

---
+++ BOJ 2751.py
@@ -0,0 +1,15 @@
+# 정렬
+
+import sys
+
+l = []
+n = int(sys.stdin.readline())
+
+for i in range(n):
+    N = int(sys.stdin.readline())
+    l.append(N)
+    
+l.sort()
+
+for i in range(n):
+    print(l[i])
